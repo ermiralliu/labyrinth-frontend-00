96c809687cd7b35c046c2965b96e27dc52a93e34
ermiralliu <mirialliu@gmail.com>
2024-10-05 03:51:24 +0200

    new game resets the boardName

--- src/Game.tsx
+++ src/Game.tsx
@@ -57,6 +57,7 @@ export default function Game(props: {
     setFirstMove(true);
     const next_board = generateBoard();
     setBoard(next_board);
+    setBoardName('default');
     setBoardId(null);
   },[]);
 
